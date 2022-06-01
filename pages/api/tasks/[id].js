33dbbb3e2a4b670ddeceb65ed70e1b63b7e46e79
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-01 22:18:49 +0200

    chore([id].js) task crud apis done

--- pages/api/tasks/[id].js
+++ pages/api/tasks/[id].js
@@ -21,6 +21,31 @@
                     return res.status(400).json({message:err.message});
                 }
 
+            case 'DELETE':
+                try{
+                    const deletedTask = await Task.findByIdAndDelete(id);
+                    if(!deletedTask) return res.status(400).json({success:false, message: "Failed to delete task", data:null});
+                    runMiddleware(req,res,morgan);
+                    return res.status(200).json({success:true, message:"Task deleted successfully", data:deletedTask});
+                }
+                catch(err){
+                    return res.status(400).json({message:err.message});
+                }    
+
+            case 'PUT':
+                try{
+                    const task = await Task.findByIdAndUpdate(id,body,{
+                        new:true,
+                        runValidators:true
+                    });
+                    if(!task) return res.status(400).json({success:false, message:'Failed to update a task', data:null});
+                    runMiddleware(req,res,morgan);
+                    return res.status(201).json({success:true,message:"Task updated successfully", data: task});
+                }   
+                catch(err){
+                    return res.status(400).json({message:err.message})
+                } 
+
             default:
                 return res.status(500).json({message:'Server error'});
         }
