99affe18f8382b059375f1d48dfd252254e32a78
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-06-02 01:01:10 +0200

    wip: adding new task working

--- pages/tasks/new.js
+++ pages/tasks/new.js
@@ -1,46 +1,96 @@
-import React, { useState } from 'react';
-import { Button, Form, Grid, Loader } from 'semantic-ui-react'
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { Button, Form, Grid, Loader } from "semantic-ui-react";
+import { useRouter } from "next/router";
 
 const CreateTask = () => {
-    const [newTask, setNewTask] = useState({
-        title: "",
-        description: ""
-    });
-    const [isSubmit, setIsSubmit] = useState(false);
-    const [errors, setErrors] = useState({});
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+  });
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [errors, setErrors] = useState({});
 
-    const { push } = useRouter();
+  const { push } = useRouter();
 
-    const {title,description} = newTask;
+  const { title, description } = newTask;
 
-    const handleSubmit = (e)=>{
-       e.preventDefault();
-    }
+  const validate = ()=>{
+      let errors = {};
+      if(!title) errors.title = "Title is required"
+      if(!description) error.description = "Description is required"
+      return errors;
+  }
 
-    const handleChange = (e)=>{
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    let errors = validate()
+    if(Object.keys(errors).length) return setErrors(errors);
+    setIsSubmit(true);
+    await createTask();
+    await push("/")
+  };
 
-    }
+  const handleChange = (e) => {
+      const {name,value} = e.target;
+      setNewTask({...newTask,[name]:value});
+  };
 
-    return (
-        <Grid centered verticalAlign='middle' columns={3} style={{ height: "80vh" }}>
-            <Grid.Row>
-                <Grid.Column textAlign='center'>
-                   <div>
-                       <h1>Create Task</h1>
-                       {isSubmit ? (<Loader active inline="centered" />):(
-                           <Form onSubmit={handleSubmit}>
-                                 <Form.Input label="Title" placeholder="Enter title" name="title"  onChange={handleChange} value={title} autofocus />
-                                 <Form.TextArea label="description" placeholder="Enter description" name="description" onChange={handleChange} value={description} autofocus>
+  const createTask = async()=>{
+      try{
+          await fetch("http://localhost:3000/api/tasks",{
+              method:"POST",
+              headers:{
+                  "content-Type":"application/json"
+              },
+              body:JSON.stringify(newTask)   
+          })
+      }
 
-                                 </Form.TextArea>
-                           </Form>
-                       )}
-                   </div>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    )
-}
+      catch(err){
+          throw new Error("Error found while creating new task ", err);
+      }
+  }
+
+  return (
+    <Grid
+      centered
+      verticalAlign="middle"
+      columns={3}
+      style={{ height: "80vh" }}
+    >
+      <Grid.Row>
+        <Grid.Column textAlign="center">
+          <div>
+            <h1>Create Task</h1>
+            {isSubmit ? (
+              <Loader active inline="centered" />
+            ) : (
+              <Form onSubmit={handleSubmit}>
+                <Form.Input
+                  label="Title"
+                  placeholder="Enter title"
+                  name="title"
+                  onChange={handleChange}
+                  value={title}
+                  autofocus
+                />
+                <Form.TextArea
+                  label="description"
+                  placeholder="Enter description"
+                  name="description"
+                  onChange={handleChange}
+                  value={description}
+                  autofocus
+                />
+
+                <Button type="submit" primary> Submit</Button>
+              </Form>
+            )}
+          </div>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
 
 export default CreateTask;
